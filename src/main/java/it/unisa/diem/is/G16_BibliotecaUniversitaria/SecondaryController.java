6f688f7a2485447d6a644b1e55136b94074e7d02
andre <andre@172.19.151.175>
2025-11-19 18:18:25 +0100

    Initial version

---
+++ src/main/java/it/unisa/diem/is/G16_BibliotecaUniversitaria/SecondaryController.java
@@ -0,0 +1,12 @@
+package it.unisa.diem.is.G16_BibliotecaUniversitaria;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module it.unisa.diem.is.G16_BibliotecaUniversitaria {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.unisa.diem.is.G16_BibliotecaUniversitaria to javafx.fxml;
+    exports it.unisa.diem.is.G16_BibliotecaUniversitaria;
+}
+*/
