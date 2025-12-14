0683665b0b5b0d1ca7f6b9b8db82c789a2ed8e1f
zamos <zamos@Ionela.Home>
2025-12-14 16:52:49 +0100

    Piccole modifiche delle classi di test

--- src/test/java/gestioneLibri/ArchivioLibriTest.java
+++ src/test/java/gestioneLibri/ArchivioLibriTest.java
@@ -1,19 +1,21 @@
 package gestioneLibri;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ArchivioLibriTest {
     
     private ArchivioLibri archivio;
-    	private Libro libro1;
-    	private Libro libro2;
-    	private Libro libro3;
+    private Libro libro1;
+    private Libro libro2;
+    private Libro libro3;
     
     @BeforeEach
     public void setUp() {
+        
         archivio = new ArchivioLibri();
     	libro1 = new Libro("La Divina Commedia", "Mondadori", 2016, "9788804671657", 5);
 	libro2 = new Libro("Divergent", "Veronica Roth", 2011,"9791221213614", 1);
@@ -25,6 +27,7 @@ public class ArchivioLibriTest {
 
     @Test
     public void testAggiungiLibroSuccesso() {
+        
         archivio.aggiungiLibro(libro3);
     	assertEquals(3, archivio.getTutti().size());
     	assertNotNull(archivio.cercaPerIsbn("9788893814522"));
@@ -32,6 +35,7 @@ public class ArchivioLibriTest {
     
     @Test
     public void testAggiungiLibroNull() {
+        
         assertThrows(IllegalArgumentException.class, () -> {
             archivio.aggiungiLibro(null);
         });
@@ -39,6 +43,7 @@ public class ArchivioLibriTest {
     
     @Test
     public void testAggiungiLibroDuplicato() {
+        
         assertThrows(IllegalArgumentException.class, () -> {
             archivio.aggiungiLibro(libro1);
         });
@@ -49,10 +54,8 @@ public class ArchivioLibriTest {
     @Test
     public void testModificaLibroSuccesso() {
 
-        Libro modificato = new Libro("La Divina Commedia-Edizione Speciale", "Mondadori", 2016, "9788804671657", 10);
-
+        Libro modificato = new Libro("La Divina Commedia-Edizione Speciale", "Mondadori", 2025, "9788804671657", 10);
 	archivio.modificaLibro(modificato);
-
 	Libro risultato = archivio.cercaPerIsbn("9788804671657");
         assertEquals("La Divina Commedia-Edizione Speciale", risultato.getTitolo());
         assertEquals(10, risultato.getCopieDisponibili());
@@ -60,14 +63,16 @@ public class ArchivioLibriTest {
     
     @Test
     public void testModificaLibroNonEsistente() {
-        Libro libroInesistente = new Libro("Inesistente", "Nessuno", 2000,"9999999999999", 1);
-        assertThrows(IllegalStateException.class, () -> {
-            archivio.modificaLibro(libroInesistente);
+        
+        Libro inesistente = new Libro("Inesistente", "Nessuno", 0000, "0000000000000", 0);
+        assertThrows(NoSuchElementException.class, () -> {
+            archivio.modificaLibro(inesistente);
         });
     }
 
     @Test
     public void testRimuoviLibroSuccesso() {
+        
         archivio.rimuoviLibro(libro1);
         assertEquals(1, archivio.getTutti().size());
         assertNull(archivio.cercaPerIsbn("9788804671657"));
@@ -75,14 +80,16 @@ public class ArchivioLibriTest {
     
     @Test
     public void testRimuoviLibroNonEsistente() {
-        Libro libroInesistente = new Libro("Inesistente", "Nessuno", 2000,"9999999999999", 1);
-        assertThrows(IllegalStateException.class, () -> {
-            archivio.rimuoviLibro(libro1);
+        
+        Libro inesistente = new Libro("Inesistente", "Nessuno", 0000, "0000000000000", 0);
+        assertThrows(NoSuchElementException.class, () -> {
+            archivio.rimuoviLibro(inesistente);
         });
     }
 
     @Test
     public void testCercaPerIsbnEsistente() {
+        
         Libro trovato = archivio.cercaPerIsbn("9788804671657");
         assertNotNull(trovato);
         assertEquals(libro1, trovato);
@@ -90,12 +97,14 @@ public class ArchivioLibriTest {
     
     @Test
     public void testCercaPerIsbnNonEsistente() {
-        Libro nonTrovato = archivio.cercaPerIsbn("9788804671658");
-        assertNull(nonTrovato);
+        
+        Libro inesistente = archivio.cercaPerIsbn("0000000000000");
+        assertNull(inesistente);
     }
     
     @Test
     public void testCercaPerIsbnNonValido() {
+        
         assertThrows(IllegalArgumentException.class, () -> {
             archivio.cercaPerIsbn("");
         });
@@ -103,19 +112,23 @@ public class ArchivioLibriTest {
 
     @Test
     public void testCercaPerTitolo() {
+        
         List<Libro> risultati = archivio.cercaPerTitolo("Divergent");
         assertEquals(1, risultati.size());
-        assertEquals(libro2, risultati.get(0));;
+        assertEquals(libro2, risultati.get(0));
     }
 
     @Test
     public void testCercaPerAutore() {
+        
         List<Libro> risultati = archivio.cercaPerAutore("Mondadori");
         assertEquals(1, risultati.size());
+        assertEquals(libro1, risultati.get(0));
     }
 
     @Test
     public void testGetLibriOrdinatiPerTitolo() {
+        
         archivio.aggiungiLibro(libro3);
         List<Libro> ordinati = archivio.getLibriOrdinatiPerTitolo();
 
@@ -126,10 +139,11 @@ public class ArchivioLibriTest {
 
     @Test
     public void testGetTutti() {
+        
+        archivio.aggiungiLibro(libro3);
         List<Libro> lista = archivio.getTutti();
         lista.clear();
-
-        assertEquals(2, archivio.getTutti().size());
+        assertEquals(3, archivio.getTutti().size());
     }
     
 }
--- src/test/java/gestioneLibri/LibroTest.java
+++ src/test/java/gestioneLibri/LibroTest.java
@@ -1,9 +1,9 @@
 package gestioneLibri;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
 
 public class LibroTest {
     
@@ -11,11 +11,13 @@ public class LibroTest {
     
     @BeforeEach
     public void setUp() {
-        libroDiProva = new Libro("Divergent", "Veronica Roth", 2011,"9791221213614", 7);
+        
+        libroDiProva = new Libro("Divergent", "Veronica Roth", 2011, "9791221213614", 7);
     }
 
     @Test
     public void testCostruttoreEGetter() {
+        
         assertEquals("Divergent", libroDiProva.getTitolo());
         assertEquals("Veronica Roth", libroDiProva.getAutore());
         assertEquals(2011, libroDiProva.getAnnoPubblicazione());
@@ -25,6 +27,7 @@ public class LibroTest {
     
     @Test
     public void testSetter() {
+        
         String nuovoTitolo = "Divergent-Edizione Speciale";
         String nuovoAutore = "Mondadori";
         int nuovoAnno = 2025;
@@ -38,25 +41,28 @@ public class LibroTest {
         assertEquals(nuovoTitolo, libroDiProva.getTitolo());
         assertEquals(nuovoAutore, libroDiProva.getAutore());
         assertEquals(nuovoAnno, libroDiProva.getAnnoPubblicazione());
-        assertEquals(nuoveCopie, libroDiProva.getCopieDisponibili());
-        
         assertEquals("9791221213614", libroDiProva.getIsbn());
+        assertEquals(nuoveCopie, libroDiProva.getCopieDisponibili());
+
     }
 
     @Test
     public void testHaCopieTrue() {
+        
         libroDiProva.setCopieDisponibili(7);
         assertTrue(libroDiProva.haCopie());
     }
     
     @Test
     public void testHaCopieFalse() {
+        
         libroDiProva.setCopieDisponibili(0);
         assertFalse(libroDiProva.haCopie());
     }
 
     @Test
     public void testIncrementaCopie() {
+        
         libroDiProva.setCopieDisponibili(7);
         libroDiProva.incrementaCopie();
         assertEquals(8, libroDiProva.getCopieDisponibili());
@@ -64,6 +70,7 @@ public class LibroTest {
 
     @Test
     public void testDecrementaCopie() {
+        
         libroDiProva.setCopieDisponibili(7);
         libroDiProva.decrementaCopie();
         assertEquals(6, libroDiProva.getCopieDisponibili());
@@ -71,6 +78,7 @@ public class LibroTest {
 
     @Test
     public void testToString() {
+        
         String risultato = libroDiProva.toString();
         assertTrue(risultato.contains("Divergent"));
         assertTrue(risultato.contains("9791221213614"));
@@ -78,28 +86,31 @@ public class LibroTest {
 
     @Test
     public void testEquals() {
+        
         //Stesso ISBN
-        Libro libroUguale = new Libro( "Titolo Diverso", "Autore Diverso", 2020, libroDiProva.getIsbn(), 9);
-        assertTrue(libroDiProva.equals(libroUguale));
+        Libro uguale = new Libro( "Titolo Diverso", "Autore Diverso", 2020, libroDiProva.getIsbn(), 9);
+        assertTrue(libroDiProva.equals(uguale));
 
         //ISBN diverso)
-        Libro libroDiverso = new Libro(libroDiProva.getTitolo(), libroDiProva.getAutore(), libroDiProva.getAnnoPubblicazione(), "9791221213614", libroDiProva.getCopieDisponibili());
-        assertFalse(libroDiProva.equals(libroDiverso));
+        Libro diverso = new Libro(libroDiProva.getTitolo(), libroDiProva.getAutore(), libroDiProva.getAnnoPubblicazione(), "0000000000000", libroDiProva.getCopieDisponibili());
+        assertFalse(libroDiProva.equals(diverso));
     }
 
     @Test
     public void testHashCode() {
+        
         // stesso ISBN
         Libro libroUguale = new Libro( "Titolo Diverso", "Autore Diverso", 2020, libroDiProva.getIsbn(), 9);
         assertEquals(libroDiProva.hashCode(), libroUguale.hashCode());
         
         //ISBN diverso
-        Libro libroDiverso = new Libro(libroDiProva.getTitolo(), libroDiProva.getAutore(), libroDiProva.getAnnoPubblicazione(), "9791221213614", libroDiProva.getCopieDisponibili());
+        Libro libroDiverso = new Libro(libroDiProva.getTitolo(), libroDiProva.getAutore(), libroDiProva.getAnnoPubblicazione(), "0000000000000", libroDiProva.getCopieDisponibili());
         assertNotEquals(libroDiProva.hashCode(), libroDiverso.hashCode());
     }
 
     @Test
     public void testCompareTo() {
+        
         //Confronto per titolo
         Libro libroA = new Libro("Alice nel Paese delle Meraviglie", "Autore A", 2000, "A1", 1);
         Libro libroZ = new Libro("Zanna Bianca", "Autore Z", 2000, "Z1", 1);
